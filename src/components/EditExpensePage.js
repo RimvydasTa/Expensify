aa5e04dab5ab466571c2df657db99da5d31cf05b
Rimvydas Tamošiūnas <rimvydas.tamosiunas@unishop.lt>
2018-06-21 14:53:42 +0300

    Redux

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit component
-    </div>
-);
+const EditExpensePage = (props) => {
+    return (
+        <div>
+            Editing the expense with the id {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink exact={true} activeClassName="is-active"  to="/">Home</NavLink>
         <NavLink  activeClassName="is-active" to="/create">Create</NavLink>
-        <NavLink  activeClassName="is-active" to="/edit">Edit</NavLink>
+
         <NavLink  activeClassName="is-active" to="/help">Help</NavLink>
     </header>
 );
